33310fce7260f40ccc2d0379435b17904300d84b
viraj_lad <viraj.lad@gmail.com>
2018-07-12 00:40:02 +0000

    Added Restaurant title to View details button Aria label for A11y

--- js/main.js
+++ js/main.js
@@ -182,6 +182,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label','View details of ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
