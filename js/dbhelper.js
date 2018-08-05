20b4d795a9cf7368a93592f35df7311fd5609ef8
viraj_lad <viraj.lad@gmail.com>
2018-08-05 23:00:23 +0000

    Started using Fetch API to get Restaurant data

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,19 +16,14 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+      fetch(DBHelper.DATABASE_URL).then( response => {
+        if (response.status === 200) {
+          response.json().then( restaurants => callback(null, restaurants));
+        } else {
+          const error = (`Request failed. Returned status of ${response.status}`);
+          callback(error, null);
+        }
+      });
   }
 
   /**
